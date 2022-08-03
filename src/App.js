5bf79670aac1875b5a2badcc09b690f976276a32
Prasanna Nimbalkar <prasannanimbalkar@Prasannas-MacBook-Pro.local>
2022-08-03 04:13:33 -0400

    First commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+import TodoApp from "./components/TodoApp.js";
+import "./bootstrap.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <TodoApp />
+    </div>
+  );
+}
